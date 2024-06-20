2ed06047f1bf43ca2ca2d03a0a981cc510939af0
Swati Goyal <swati.goyal@jiva.ag>
2024-06-20 21:15:37 +0530

    chess game

---
+++ chess-game/chess_game.rb
@@ -0,0 +1,25 @@
+module Chess
+  RANKS = 1..8
+  FILES = 'A'..'H'
+
+  def self.valid_square?(rank, file)
+    if RANKS.include?(rank) && FILES.include?(file)
+      true
+    else
+      false
+    end
+  end
+
+  def self.nick_name(first_name, last_name)
+    (first_name[0..1] + last_name[-2..-1]).upcase
+  end
+
+  def self.move_message(first_name, last_name, square)
+    nickname = nick_name(first_name, last_name)
+    if valid_square?(square[1].to_i, square[0])
+      "#{nickname} moved to #{square}"
+    else
+      "#{nickname} attempted to move to #{square}, but that is not a valid square"
+    end
+  end
+end
